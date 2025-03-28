fa8714cad150ae0ea347157330dfee72e10ec73e
APIVerve Team <hello@apiverve.com>
2025-03-28 18:06:35 -0500

    Bug Fixes

--- src/apiverve_websitetotext/apiClient.py
+++ src/apiverve_websitetotext/apiClient.py
@@ -10,10 +10,10 @@ class WebsitetotextAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/websitetotext'
+        self.base_url = 'https://api.apiverve.com/v1/websitetotext'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
